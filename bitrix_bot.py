bacb8f1b20bcad62ff9dc024a83ff3a767e7090c
danil31219as <57491208+danil31219as@users.noreply.github.com>
2020-05-06 16:33:36 +0400

    Add files via upload

--- bitrix_bot.py
+++ bitrix_bot.py
@@ -7,10 +7,10 @@ import requests
 from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
 from vk_api.keyboard import VkKeyboard, VkKeyboardColor
 import os
-TOKEN = os.environ.get('TOKEN')
+TOKEN = str(os.environ.get('TOKEN'))
 GROUP_ID = int(os.environ.get('GROUP_ID'))
-TOKEN_BITRIX = os.environ.get('TOKEN_BITRIX')
-DOMAIN = os.environ.get('DOMAIN')
+TOKEN_BITRIX = str(os.environ.get('TOKEN_BITRIX'))
+DOMAIN = str(os.environ.get('DOMAIN'))
 logging.info('Keys are getting')
 WAY = 'UF_CRM_1588349613887'
 COLLABORATORS_LIST = [9, 11]
